a30cd745bf952a2539ed64c435a8074172a5ad0e
Eric Ashmore <edashmore@gmail.com>
2015-10-12 16:32:34 -0700

    initial commit

---
+++ lib/controller_base.rb
@@ -0,0 +1,77 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'active_support/inflector'
+require 'erb'
+
+require_relative "./session"
+
+module RailsLite
+  class ControllerBase
+    attr_reader :req, :res
+
+    # Setup the controller
+    def initialize(req, res)
+      @req = req
+      @res = res
+      @already_built_response = false
+    end
+
+    # Helper method to alias @already_built_response
+    def already_built_response?
+      @already_built_response
+    end
+
+    # Set the response status code and header
+    def redirect_to(url)
+      raise "double render error" if @already_built_response
+
+      @res.status = 302
+      @res["Location"] = url
+
+      @already_built_response = true
+      session.store_session(@res)
+
+      nil
+    end
+
+    # Populate the response with content.
+    # Set the response's content type to the given type.
+    # Raise an error if the developer tries to double render.
+    def render_content(content, content_type)
+      raise "double render error" if @already_built_response
+
+      @res.body = content
+      @res.content_type = content_type
+
+      @already_built_response = true
+
+      session.store_session(@res)
+      nil
+    end
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    path = File.dirname(__FILE__)
+    filename = File.join(path, "..", "..", "views",
+      self.class.underscore, "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(filename)
+
+    render_content(ERB.new(template_code).result(binding), "text/html")
+  end
+
+  def session(req)
+    @session ||= Session.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
+
+    nil
+  end
+end
---
+++ lib/params.rb
@@ -0,0 +1,56 @@
+require 'uri'
+
+class Params
+  # use your initialize to merge params from
+  # 1. query string
+  # 2. post body
+  # 3. route params
+  #
+  # You haven't done routing yet; but assume route params will be
+  # passed in as a hash to `Params.new` as below:
+  def initialize(req, route_params = {})
+    @params = {}
+  end
+
+  def [](key)
+  end
+
+  def to_s
+    @params.to_json.to_s
+  end
+
+  class AttributeNotFoundError < ArgumentError; end;
+
+  private
+  # this should return deeply nested hash
+  # argument format
+  # user[address][street]=main&user[address][zip]=89436
+  # should return
+  # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
+  def parse_www_encoded_form(www_encoded_form)
+    params = {}
+
+    key_values = URI.decode_www_form(www_encoded_form)
+    key_values.each do |full_key, value|
+      scope = params
+
+      key_seq = parse_key(full_key)
+      key_seq.each_with_index do |key, idx|
+        if (idx + 1) == key_seq.count
+          scope[key] = value
+        else
+          scope[key] ||= {}
+          scope = scope[key]
+        end
+      end
+    end
+
+    params
+  end
+
+  # this should return an array
+  # user[address][street] should return ['user', 'address', 'street']
+  def parse_key(key)
+    key.split(/\[|\]\[|\}/)
+  end
+end
